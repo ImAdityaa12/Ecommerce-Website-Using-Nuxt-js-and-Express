661b8adb5d45572469942f9fc591d9eb98b2846f
ImAdityaa12 <adityagupta1291@gmail.com>
2024-10-22 00:58:26 +0530

    added updateItemQuantity Controller

--- server/src/controllers/cartController.ts
+++ server/src/controllers/cartController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import cartModel from "../models/cartModel";
 import productModel from "../models/productModel";
+import { Types } from "mongoose";
 interface IProduct {
   _id: Types.ObjectId;
   image: string;
@@ -70,39 +71,6 @@ export const addToCartController = async (req: Request, res: Response) => {
   }
 };
 
-// export const fetchCartController = async (req: Request, res: Response) => {
-//   try {
-//     const cart: any = await cartModel
-//       .findOne({ userId: req.query.userId })
-//       .populate({
-//         path: "items.productId",
-//         select: "image title price salePrice",
-//       });
-//     if (!cart) {
-//       res.status(400).send("Cart not found in databse");
-//       return;
-//     }
-//     const items = cart.items.map((item: any) => ({
-//       productId: item.productId._id,
-//       title: item.productId.title,
-//       image: item.productId.image,
-//       price: item.productId.price,
-//       salePrice: item.productId.salePrice,
-//       quantity: item.quantity,
-//     }));
-//     res.status(200).json({
-//       _id: cart._id,
-//       userId: cart.userId,
-//       items,
-//     });
-//     return;
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send("An error occurred");
-//   }
-// };
-import { Types } from "mongoose";
-
 export const fetchCartController = async (
   req: Request<{}, {}, {}, { userId: string }>,
   res: Response
@@ -138,6 +106,47 @@ export const fetchCartController = async (
     res.status(200).json(response);
   } catch (error) {
     console.error(error);
+    res.status(500).send("An error occurred");
+  }
+};
+
+export const updateCartItemQuantityController = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const { userId, productId, quantity } = req.body;
+    if (!userId || !productId || !quantity) {
+      res.status(400).send("Missing required fields");
+      return;
+    }
+
+    const cart = await cartModel.findOne({ userId });
+    if (!cart) {
+      res.status(400).send("Cart not found in database");
+      return;
+    }
+
+    const findCurrentProductIndex = cart.items.findIndex(
+      (item) => item.productId.toString() === productId
+    );
+    if (findCurrentProductIndex === -1) {
+      res.status(400).send("Product not found in cart");
+      return;
+    } else {
+      if (quantity === "plus") {
+        cart.items[findCurrentProductIndex].quantity += 1;
+        await cart.save();
+      } else {
+        cart.items[findCurrentProductIndex].quantity = cart.items[
+          findCurrentProductIndex
+        ].quantity -= 1;
+        await cart.save();
+      }
+      res.status(200).json(cart);
+    }
+  } catch (error) {
+    // console.error(error);`
     res.status(500).send("An error occurred");
   }
 };
--- server/src/routes/cartRouter.ts
+++ server/src/routes/cartRouter.ts
@@ -2,8 +2,10 @@ import express from "express";
 import {
   addToCartController,
   fetchCartController,
+  updateCartItemQuantityController,
 } from "../controllers/cartController";
 const router = express.Router();
 router.get("/", fetchCartController);
 router.post("/addToCart", addToCartController);
+router.post("/updateCartItemQuantity", updateCartItemQuantityController);
 export default router;
