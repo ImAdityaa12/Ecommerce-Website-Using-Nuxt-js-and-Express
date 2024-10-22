2e38c4962f9d0059c2253f823490724f78ffe2e0
ImAdityaa12 <adityagupta1291@gmail.com>
2024-10-22 21:51:12 +0530

    Added deleteItem Route

--- server/src/routes/cartRouter.ts
+++ server/src/routes/cartRouter.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   addToCartController,
+  deleteCartItemController,
   fetchCartController,
   updateCartItemQuantityController,
 } from "../controllers/cartController";
@@ -8,4 +9,5 @@ const router = express.Router();
 router.get("/", fetchCartController);
 router.post("/addToCart", addToCartController);
 router.post("/updateCartItemQuantity", updateCartItemQuantityController);
+router.delete("/deleteCartItem", deleteCartItemController);
 export default router;
