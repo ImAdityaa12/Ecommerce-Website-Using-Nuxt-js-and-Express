d422932e0dc6186f052864ab6fc7eec112d4c518
ImAdityaa12 <adityagupta1291@gmail.com>
2024-10-24 00:48:51 +0530

    Added update, delete and getAll address API

--- server/src/routes/addressRoute.ts
+++ server/src/routes/addressRoute.ts
@@ -1,6 +1,14 @@
 import express from "express";
-import { addAddressController } from "../controllers/addressController";
+import {
+  addAddressController,
+  deleteAddressController,
+  fetchAllAddressController,
+  updateAddressController,
+} from "../controllers/addressController";
 const router = express.Router();
+router.get("/all/:userId", fetchAllAddressController);
 router.post("/add", addAddressController);
+router.put("/update/:userId/:id", updateAddressController);
+router.delete("/delete/:id", deleteAddressController);
 
 export default router;
